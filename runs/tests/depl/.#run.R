3a8a3cfb688181911943ce7c4cfa3113ff1ae7d8
Backend User for OpenBiodiv Deployment <backend@obkms.pensoft.local>
2018-06-08 12:33:58 +0300

    deployment test

--- runs/tests/depl/.#run.R
+++
@@ -1,1 +0,0 @@
-backend@obkms.pensoft.local.2609:1528444880
--- runs/tests/depl/run.R
+++ runs/tests/depl/run.R
@@ -1,36 +1,27 @@
-# Iteration 8
+# Test Deployment
 library(rdf4r)
 library(ropenbio)
 
 obkms = basic_triplestore_access(
-  server_url = "192.168.83.16:7777",
-  repository = "obkms_i8",
+  server_url = "192.168.90.23:7200",
+  repository = "depl_test",
   user = "admin",
   password = Sys.getenv("OBKMS")
 )
 
-#list_repositories(obkms)
-obkms$repository = "obkms_i8"
+list_repositories(obkms)
 obkms$prefix = c(openbiodiv = "http://openbiodiv.net/")
 
-# Process Plazi
-taxonx_files = list.files(
-  "/home/viktor2/Work/PhD/openbiodiv/runs/iteration/8/plazi-test",
-  full.names = TRUE,
-  pattern = "*.xml"
-)
-
-processing_results = sapply(taxonx_files, xml2rdf, access_options = obkms, serialization_dir = "/home/viktor2/Work/PhD/openbiodiv/runs/iteration/8/ttl-test", reprocess = TRUE)
-
-# Process Plazi
+# Process Pensoft Test
 pensoft_files = list.files(
-  "/home/viktor2/Work/PhD/openbiodiv/runs/iteration/8/pensoft-test//",
+  "~/OpenBiodiv/runs/tests/depl/pensoft-test",
   full.names = TRUE,
   pattern = "*.xml"
 )
 
 
-pensoft_processing_results = sapply(pensoft_files, xml2rdf, xml_schema = taxpub, access_options = obkms, serialization_dir = "/home/viktor2/Work/PhD/openbiodiv/runs/iteration/8/ttl-test", reproces = TRUE)
+proc_res = sapply(pensoft_files, xml2rdf, xml_schema = taxpub, access_options = obkms,
+serialization_dir = "~/OpenBiodiv/runs/tests/depl/ttl-test", reproces = TRUE)
 
 
 # fails
